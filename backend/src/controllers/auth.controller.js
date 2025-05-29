230e98c687911704e15da6c582a3cf96ab6959fe
Rajdeep <rajdeepmitra8@gmail.com>
2025-05-29 23:52:12 +0530

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -97,8 +97,8 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.SAME_SITE, // prevent CSRF attacks
+      secure: process.env.secure === "true",
     });
 
     res.status(200).json({ success: true, user });
